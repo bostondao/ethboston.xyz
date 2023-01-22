9dc3b20b5b4ac455c45d8e45ab2a83666b1dd4e3
jackdishman <me@jackdishman.com>
2023-01-22 09:07:04 -0500

    style: make it closer to old site

--- src/App.js
+++ src/App.js
@@ -1,44 +1,49 @@
 
 function App() {
   return (
-    <main>
+    <main style={ { backgroundColor: `rgba(252, 189, 75, .1)` } }>
       {/* Logo, header, image */ }
       <section className="max-w-full p-5">
-        <article className="flex justify-center items-center">
-          <img src="./logo.svg" alt="ETH boston logo" className="h-12 mr-4" />
-          <img src="./text-ethboston.png" alt="title" className="h-12" />
+        <article>
+          <div className="flex justify-center">
+            <img src="./logo.svg" alt="ETH boston logo" className="h-48 my-4" />
+          </div>
+          <div className="flex justify-center">
+            <img src="./text-ethboston.png" alt="title" className="h-24" />
+          </div>
         </article>
       </section>
-      {/* Cover image */ }
-      <section className="flex justify-center h-full items-center">
-        <article className="p-5">
-          <img src="./bg.png" alt="ETH Boston coming April 2023" className="rounded-lg border-2 border-black" />
-        </article>
-      </section>
-      {/* Below image */ }
+      {/* Socials */ }
       <section className="flex justify-center">
         <article className="flex items-center justify-around p-5 w-full sm:w-2/3">
           {/* Twitter */ }
-          <a href="https://twitter.com/ETHBoston" target="_blank" rel="noreferrer" className="flex items-center  text-xl rounded-lg border-2 border-black px-4 py-2 hover:bg-sky-100">Twitter<img src="./twitter.svg" alt="Twitter icon" className="ml-2" /></a>
-          {/* Date and Location */ }
-          <div className="flex items-center rounded-full border justify-between w-96 bg-gradient-to-r from-gray-100 to-sky-100">
+          <a href="https://twitter.com/ETHBoston" target="_blank" rel="noreferrer" className="flex items-center  text-xl rounded-full border-2 border-black px-4 py-2 hover:bg-sky-100">Follow us on<img src="./twitter.svg" alt="Twitter icon" className="ml-2" /></a>
+          {/* Date and location */ }
+          <div className="flex items-center rounded-full border justify-between bg-gradient-to-r from-gray-100 to-sky-100">
             <article className="flex items-center justify-center">
-              <span className="rounded-full border p-2 bg-sky-100"><img src="./calendar.svg" alt="Dates" /></span>
-              <span className="text-xl mx-2 font-semibold">April 7-9 2023</span>
+              <span className="rounded-full border p-2 m-1 bg-sky-100"><img src="./calendar.svg" alt="Dates" /></span>
+              <span className="text-2xl mx-2 pr-2 font-semibold">April 7-9 2023</span>
             </article>
             <article className="flex items-center justify-center">
-              <span className="text-xl mx-2 font-semibold">Revere Hotel</span>
-              <span className="rounded-full border p-2 bg-gray-100">
+              <span className="text-2xl mx-2 pl-2 font-semibold">Revere Hotel</span>
+              <span className="rounded-full border p-2 m-1 bg-gray-100">
                 <img src="./map-pin.svg" alt="Dates" />
               </span>
             </article>
           </div>
           {/* Email */ }
-          <a href="mailto:sponsor@ethboston.xyz" target="_blank" rel="noreferrer" className="flex items-center  text-xl rounded-lg border-2 border-black px-4 py-2 hover:bg-gray-100">Inquire<img src="./mail.svg" alt="Twitter icon" className="ml-2" /></a>
+          <a href="mailto:sponsor@ethboston.xyz" target="_blank" rel="noreferrer" className="flex items-center  text-xl rounded-full border-2 border-black px-4 py-2 hover:bg-gray-100">Inquire<img src="./mail.svg" alt="Twitter icon" className="ml-2" /></a>
         </article>
       </section>
+      {/* Cover image */ }
+      <section className="flex justify-center h-full items-center">
+        <article className="px-5 mt-5">
+          <img src="./bg.png" alt="ETH Boston coming April 2023" className="" />
+        </article>
+      </section>
+      {/* Below image */ }
       {/* Description */ }
-      <section className="flex justify-center">
+      <section className="flex justify-center bg-sky-100">
         <article className="p-5 w-full sm:w-2/3">
           <p className="mb-4">ETHBoston and The Boston DAO are proud to present ETHBoston 2023 at the Revere
             Hotel April 7-9 2023. We are excited about the return of this conference since the last
