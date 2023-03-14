0cab7aadba1457cec983c1095783dcf37cd4d670
jackdishman <me@jackdishman.com>
2023-03-14 15:11:50 -0500

    style: remove bg color

--- src/components/SpeakerCard.jsx
+++ src/components/SpeakerCard.jsx
@@ -1,6 +1,6 @@
 export default function SpeakerCard(props) {
   return (
-    <div className="border-2 rounded-lg bg-sky-100 shadow-md py-2">
+    <div className="border border-sky-100 rounded-lg shadow-md py-2">
       <h2 className="text-center text-2xl text-blue-1">{props.name}</h2>
       <div className="flex justify-center my-2">
         <img src={`./speakers/${props.imgSrc}`} alt={props.name} className="object-cover w-64 h-64 rounded-lg" />
