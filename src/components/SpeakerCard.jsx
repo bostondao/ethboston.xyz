08c3c5d89bd7c2f4896f59db1ba4baaa1b7d01d0
jackdishman <me@jackdishman.com>
2023-04-04 17:22:41 -0400

    feat: add links

--- src/components/SpeakerCard.jsx
+++ src/components/SpeakerCard.jsx
@@ -6,11 +6,9 @@ export default function SpeakerCard(props) {
       <div className="flex justify-center">
         <img src={`./speakers/${props.imgSrc}`} alt={props.name} className="object-cover w-full h-64 rounded-t-lg" />
       </div>
-      <div className="p-4 flex justify-between">
-        <div className="">
-          <h2 className="text-3xl font-semibold text-white font-serif">{props.name}</h2>
-          <h6 className="text-red-2 text-lg font-serif font-semibold">{props.company}</h6>
-        </div>
+      <h2 className="text-3xl pt-4 px-4 font-semibold text-white font-serif">{props.name}</h2>
+      <div className="px-4 pb-4 flex justify-between h-auto items-stretch">
+        <h6 className="text-red-2 text-lg font-serif font-semibold">{props.company}</h6>
         { props.linkedin ? 
           <a href={props.linkedin} target="_blank" referrerPolicy="norefer" className="text-red-2 flex self-end h-full pl-4" rel="noreferrer">
           <LinkedinIcon />
