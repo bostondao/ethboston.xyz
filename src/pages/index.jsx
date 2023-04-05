081f9aa62bf0ac5ce519465456f445efeb73919d
jackdishman <me@jackdishman.com>
2023-04-05 15:32:16 -0400

    fix: make images square

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -143,7 +143,7 @@ export default function Index() {
 						Speakers
 					</h1>
 				</div>
-				<article className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 p-10">
+				<article className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-y-10 p-10">
 					{speakerList.speakers.map((s) => {
 						return (
 							<SpeakerCard
