0dea8423bfbfc73f35464d5bcf08bb1b0b2f609b
jackdishman <me@jackdishman.com>
2023-04-04 20:26:49 -0400

    style: align linkedin icon

--- src/components/SpeakerCard.jsx
+++ src/components/SpeakerCard.jsx
@@ -2,16 +2,16 @@ import LinkedinIcon from "./icons/LinkedinIcon"
 
 export default function SpeakerCard(props) {
   return (
-    <div className="bg-blue-3 rounded-lg shadow-md">
+    <div className="bg-blue-3 rounded-lg shadow-md flex flex-col relative">
       <div className="flex justify-center">
         <img src={`./speakers/${props.imgSrc}`} alt={props.name} className="object-cover w-full h-64 rounded-t-lg" />
       </div>
       <h2 className="text-3xl pt-4 px-4 font-semibold text-white font-serif">{props.name}</h2>
-      <div className="px-4 pb-4 flex justify-between h-auto items-stretch">
-        <h6 className="text-red-2 text-lg font-serif font-semibold">{props.company}</h6>
+      <div className="px-4 pb-4 flex justify-between self-stretch">
+        <h6 className={`text-red-2 text-lg font-serif font-semibold ${props.linkedin && `pr-12`}`}>{props.company}</h6>
         { props.linkedin ? 
-          <a href={props.linkedin} target="_blank" referrerPolicy="norefer" className="text-red-2 flex self-end h-full pl-4" rel="noreferrer">
-          <LinkedinIcon />
+          <a href={props.linkedin} target="_blank" referrerPolicy="norefer" className="text-red-2 absolute right-0 bottom-0 p-4" rel="noreferrer">
+            <LinkedinIcon />
           </a>
         : null
       }
