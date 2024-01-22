1dece172eb3268c26d6b525a385bda95468db7c9
Jake Lynch <56452947+lakejynch@users.noreply.github.com>
2024-01-22 16:22:13 -0500

    Update index.jsx to say "previous speakers"

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -167,7 +167,7 @@ export default function Index() {
 			<section className="py-10" id="speakers">
 				<div className="flex justify-center">
 					<h1 className="text-5xl uppercase font-semibold text-blue-1">
-						Speakers
+						Previous Speakers
 					</h1>
 				</div>
 				<article className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-y-10 p-10">
