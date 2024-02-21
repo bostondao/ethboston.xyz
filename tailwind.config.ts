ed4bab75b449dcbed01a664d0e5965d2b1341015
Jack Dishman <me@jackdishman.com>
2024-02-21 17:06:45 -0500

    fix: convert to nextjs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,10 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+const config: Config = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -25,4 +28,5 @@ module.exports = {
     },
   },
   plugins: [],
-}
+};
+export default config;
